16c326257ac9b04a9784279745066eea2c8b3ab5
Esma Sherwani <esmasherr@gmail.com>
2020-12-02 00:53:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,17 @@
 def join_nested_strings(src)
-  separated_array = array.partition { |item| item.is_a? String }
+  
+  mixed_data_1 = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+mixed_data_2 = [
+  ["Turning", "and", "turning", "in", "the", "widening", "gyre"],
+  ["The", "falcon", "cannot", "hear", "the", "falconer;"],
+  ["Things", "fall", "apart;", "the", "centre", "cannot", "hold"]
+]
+  separated_array = mixed_data_1.partition { |item| item.is_a? String }
   separated_array
 end
  
