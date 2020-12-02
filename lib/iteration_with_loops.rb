baa1e80e7bc6c24b10215707141277edb0e27d0c
Esma Sherwani <esmasherr@gmail.com>
2020-12-02 00:49:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,2 @@
-def join_nested_strings(src)
- return "The quick brown fox studied Ruby"
- return "Turning and turning in the widening gyre The falcon cannot hear the falconer; Things fall apart; the centre cannot hold"
-end
+
  
