6927fd7aa071dbe7a39f4da55beb7355ffcdf1d0
Esma Sherwani <esmasherr@gmail.com>
2020-12-02 00:46:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,17 @@
 def join_nested_strings(src)
- mixed_data.flatten.select{ |x| x.is_a? String }.join " " 
+ mixed_data = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+acc = {strings: [], integers: []}
+mixed_data.flatten.reduce(acc) do |acc, item|
+  acc[:strings].push(item) if item.is_a? String
+  acc[:integers].push(item) if item.is_a? Integer
+  acc
+end
+
+p acc
+p acc[:strings].length
 end
