bbd2dc0ac1af14da55a6a4a2eb2e8d8f6cfde3c1
Esma Sherwani <esmasherr@gmail.com>
2020-12-02 00:51:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,23 @@
 def def join_nested_strings(src)
+  string_result = [ ]
+  row_index = 0
+  while row_index < src.count do
+  element_index = 0
+  string_elements = ""
+  while element_index < src[row_index].count do
+  if src[row_index][element_index]
+  string_result << src[row_index][element_index]
+  element_index
+  end
+  element_index += 1
+  end
+
+row_index += 1
+
+end
+
+string_result.join
+
 
 end
  
